e149bd8a6afaf4253239ac497ca3e36dca5674ac
Varnika Malik <varnikamalik2511@gmail.com>
2026-06-02 22:17:56 +0530

    maximumsubarray.java

---
+++ week 1/day 1/day 2/maximumsubarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int currSum=0, maxSum=nums[0];
+        for(int i=0;i<nums.length;i++){
+            currSum += nums[i];
+            maxSum = Math.max(maxSum,currSum);
+            if(currSum<0){
+                currSum=0;
+            }
+        }
+        return maxSum;
+    }
+}
