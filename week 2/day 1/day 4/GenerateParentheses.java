abd4666ecc813aa5db348a0fc6adb3b8e112ca25
Varnika Malik <varnikamalik2511@gmail.com>
2026-06-11 18:23:21 +0530

    Create GenerateParentheses.java

---
+++ week 2/day 1/day 4/GenerateParentheses.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList<>();
+        backtrack(ans, "", 0, 0, n);
+        return ans;
+    }
+    private void backtrack(List<String> ans,String current, int open, int close,int n) {
+        if(current.length() == 2 * n){
+            ans.add(current);
+            return;
+        }
+        if (open < n){
+            backtrack(ans, current + "(", open + 1, close, n);
+        }
+        if (close < open){
+            backtrack(ans, current + ")", open, close + 1, n);
+        }
+    }                     
+}
