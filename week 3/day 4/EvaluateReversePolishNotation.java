69a7e4703fbad236df94b9cd79a2654a0177762f
Varnika Malik <varnikamalik2511@gmail.com>
2026-06-18 22:27:26 +0530

    EvaluateReversePolishNotation.java

---
+++ week 3/day 4/EvaluateReversePolishNotation.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack = new Stack<>();
+        for (String token : tokens) {
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                int b = stack.pop();
+                int a = stack.pop();
+                switch (token) {
+                    case "+":
+                        stack.push(a + b);
+                        break;
+
+                    case "-":
+                        stack.push(a - b);
+                        break;
+
+                    case "*":
+                        stack.push(a * b);
+                        break;
+
+                    case "/":
+                        stack.push(a / b);
+                        break;
+                }
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+        return stack.peek();
+    }
+}
