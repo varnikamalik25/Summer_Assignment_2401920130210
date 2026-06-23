23db958e9be23efe597b5cbf68443074a55f8c34
Varnika Malik <varnikamalik2511@gmail.com>
2026-06-23 21:57:13 +0530

    Create BinaryTreeLevelOrderTraversal.java

---
+++ week 4/day 2/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                level.add(curr.val);
+                if (curr.left != null) {
+                    q.offer(curr.left);
+                }
+                if (curr.right != null) {
+                    q.offer(curr.right);
+                }
+            }
+            ans.add(level);
+        }
+        return ans;
+    }
+}
