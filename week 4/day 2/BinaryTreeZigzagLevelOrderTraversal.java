1ac2139b0d0b4bea6919d7c4b95633135cadc38d
Varnika Malik <varnikamalik2511@gmail.com>
2026-06-23 22:10:13 +0530

    Create BinaryTreeZigzagLevelOrderTraversal.java

---
+++ week 4/day 2/BinaryTreeZigzagLevelOrderTraversal.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null)
+            return ans;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        boolean leftToRight = true;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            LinkedList<Integer> level = new LinkedList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = q.poll();
+
+                if (leftToRight) {
+                    level.addLast(node.val);
+                } else {
+                    level.addFirst(node.val);
+                }
+                if (node.left != null) {
+                    q.offer(node.left);
+                }
+                if (node.right != null) {
+                    q.offer(node.right);
+                }
+            }
+            ans.add(level);
+            leftToRight = !leftToRight;
+        }
+        return ans;
+    }
+}
